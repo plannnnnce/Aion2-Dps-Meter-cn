ddff655e0f51e6454a195eabb33ca5d105cc34aa
TK-open-public <itaebread2@gmail.com>
2026-01-06 03:55:18 +0900

    Feat: DataStorage 닉네임 저장소 추가

--- src/main/kotlin/DataStorage.kt
+++ src/main/kotlin/DataStorage.kt
@@ -2,16 +2,17 @@ package com.tbread
 
 class DataStorage {
     private val damageStorage = HashMap<String, HashMap<String, MutableSet<ParsedDamagePacket>>>()
-    private val nicknameStorage = HashMap<Int,String>()
+    private val nicknameStorage = HashMap<Int, String>()
 
     fun appendDamage(pdp: ParsedDamagePacket) {
-        damageStorage.getOrPut("${pdp.getActorId()}"){ hashMapOf() }
-            .getOrPut("${pdp.getTargetId()}"){ hashSetOf() }
+        damageStorage.getOrPut("${pdp.getActorId()}") { hashMapOf() }
+            .getOrPut("${pdp.getTargetId()}") { hashSetOf() }
             .add(pdp)
     }
 
-    fun appendNickname() {
-        //추후 추가
+    fun appendNickname(uid: Int, nickname: String) {
+        if (nicknameStorage[uid] != null && nicknameStorage[uid] == nickname) return
+        println("$uid 할당 닉네임 변경됨 이전: ${nicknameStorage[uid]} 현재: $nickname")
     }
 
     private fun flushDamageStorage() {
