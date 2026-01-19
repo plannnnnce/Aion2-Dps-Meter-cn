006d06fe5821625ee2879889b5167a857b142f37
TK-open-public <itaebread2@gmail.com>
2026-01-19 16:43:42 +0900

    Feat: 수호성 스킬코드 검수 완료
    issue #2

--- src/main/kotlin/entity/JobClass.kt
+++ src/main/kotlin/entity/JobClass.kt
@@ -2,7 +2,7 @@ package com.tbread.entity
 
 enum class JobClass(val className: String, val basicSkillCode: Int) {
     GLADIATOR("검성", 11020000),
-    TEMPLAR("수호성", 12020000),
+    TEMPLAR("수호성", 12010000),
     RANGER("궁성", 14020000),
     ASSASSIN("살성", 13010000),
     SORCERER("마도성", 15210000), /* 마도 확인 필요함 */
